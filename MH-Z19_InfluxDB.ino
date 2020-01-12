1d35188d04af717458da47b0e3e73b1a4bde961b
bfaliszek <blazejfaliszek@gmail.com>
2020-01-12 09:42:50 +0100

    added more calibration options

--- MH-Z19_InfluxDB.ino
+++ MH-Z19_InfluxDB.ino
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include "src/MHZ19.h"                                     // https://github.com/WifWaf/MH-Z19 // updated - 10.01.2020
+#include "src/MHZ19.h"                                  // https://github.com/WifWaf/MH-Z19 // updated - 10.01.2020
 #include "src/InfluxDb.h"                               // https://github.com/tobiasschuerg/ESP8266_Influx_DB // updated - 10.01.2020
 
 int READ_DATA_EVERY_SECONDS = 20; // default 20 sec
@@ -12,15 +12,17 @@ int SEND_DATA_EVERY_SECONDS = 60; // default 60 sec
 #define DEVICE_NAME "XXX"
 
 // WiFi Config
-#define WiFi_SSID "SSID"
-#define WiFi_Password "PASSWORD"
+#define WiFi_SSID "Bambrony"
+#define WiFi_Password "QJ3-cfV@yTG4zaQ"
 
-#ifdef ARDUINO_ARCH_ESP32 // D17 - D16
-#define RX_PIN 16                                          // Rx pin which the MHZ19 Tx pin is attached to
-#define TX_PIN 17                                          // Tx pin which the MHZ19 Rx pin is attached to
-#else                     // D1 - D2
-#define RX_PIN 4                                           // Rx pin which the MHZ19 Tx pin is attached to
-#define TX_PIN 5                                           // Tx pin which the MHZ19 Rx pin is attached to
+#define MHZ19_AUTOCALIBRATION_ON // Turn ON or OFF(just delete or comment this line)
+
+#ifdef ARDUINO_ARCH_ESP32
+#define RX_PIN 16          // D16                        
+#define TX_PIN 17          // D17
+#else
+#define RX_PIN 4           // D2                                     
+#define TX_PIN 5           // D1                               
 #endif
 
 #ifdef ARDUINO_ARCH_ESP32
@@ -71,6 +73,49 @@ void send_data() {
   influx.write(row);
 }
 
+void mhz19_calibration() {
+  unsigned long getDataTimer = 0;
+  void verifyRange(int range);
+
+  /*            ### setRange(value)###
+         Basic:
+         setRange(value) - set range to value (advise 2000 or 5000).
+         setRange()      - set range to 2000.
+         Advanced:
+         Use verifyRange(int range) from this code at the bottom.
+  */
+
+  myMHZ19.setRange(5000);
+
+  /*            ###calibrateZero()###
+     Basic:
+     calibrateZero() - request zero calibration
+     Advanced:
+     In Testing.
+  */
+
+  // myMHZ19.calibrateZero(); // Always OFF!!! Experimental!
+
+  /*             ### setSpan(value)###
+     Basic:
+     setSpan(value) - set span to value (strongly recommend 2000)
+     setSpan()      - set span to 2000;
+  */
+
+  myMHZ19.setSpan(2000);
+
+  /*            ###autoCalibration(false)###
+     Basic:
+     autoCalibration(false) - turns auto calibration OFF. (automatically sent before defined period elapses)
+     autoCalibration(true)  - turns auto calibration ON.
+     autoCalibration()      - turns auto calibration ON.
+     Advanced:
+     autoCalibration(true, 12) - turns autocalibration ON and calibration period to 12 hrs (maximum 24hrs).
+  */
+
+  myMHZ19.autoCalibration(false);
+}
+
 void setup()
 {
   Serial.begin(115000);                                    // Device to serial monitor feedback
@@ -100,8 +145,14 @@ void setup()
 #endif
   myMHZ19.begin(myMHZ19Serial);                                 // *Serial(Stream) refence must be passed to library begin().
 
-  myMHZ19.autoCalibration();                             // Turn auto calibration ON (OFF autoCalibration(false))
+#ifdef MHZ19_AUTOCALIBRATION_ON
+  myMHZ19.autoCalibration(true);                             // Turn auto calibration ON (OFF autoCalibration(false))
+#else
+  mhz19_calibration();                                       // Manual calibration
   //myMHZ19.autoCalibration(false);                          // Turn auto calibration OFF
+#endif
+
+  Serial.print("Auto Base Calibration(ABC) Status: "); myMHZ19.getABC() ? Serial.println("ON") :  Serial.println("OFF");
 
   take_measurements();
 }
@@ -122,4 +173,17 @@ void loop()
     previousMillis_SENDING = millis();
   }
 
+}
+
+void verifyRange(int range)
+{
+  Serial.println("Requesting new range.");
+
+  myMHZ19.setRange(range);                             // request new range write
+
+  if (myMHZ19.getRange() == range)                     // Send command to device to return it's range value.
+    Serial.println("Range successfully applied.");   // Success
+
+  else
+    Serial.println("Failed to apply range.");        // Failed
 }
